cd677401fadd6e0bd53fad0778bd54cfa730ed83
Crisley Alves <crisleyalvesphx@gmail.com>
2022-02-01 11:13:34 -0300

    Add basic marvel api setup to make requests

---
+++ src/config/index.js
@@ -0,0 +1,5 @@
+export const MARVEL_API_BASE_URL = 'http://gateway.marvel.com/v1/public/'
+
+export const API_KEY = 'e987c2d8e1c6d7290bebea07a090ac8f';
+export const API_HASH = '4ce0ed723205564788bcfa1d50c76285'
+export const MARVEL_API_AUTHORIZATION = `&apikey=${API_KEY}&hash=${API_HASH}`
